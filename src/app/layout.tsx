d84604bc92e95bbdb71744fee7126e7533dd9674
Juliendy Mendes <juliendy0708@gmail.com>
2024-01-05 12:57:04 -0400

    chore:  confugure default fonts and colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,41 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Barlow, Barlow_Condensed, Bellefair } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const barlow = Barlow({
+	weight: '400',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-barlow",
+});
+
+const barlowCondensed = Barlow_Condensed({
+	weight: '400',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-barlow-condensed",
+});
+
+const bellefair = Bellefair({
+	weight: '400',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bellefair",
+});
 
 export const metadata: Metadata = {
-  title: 'Space Tourism',
-  description: 'Travel to space',
-}
+  title: "Space Tourism",
+  description: "Travel to space",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${barlow.variable} ${barlowCondensed.variable} ${bellefair.variable}`}>{children}</body>
     </html>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+			colors: {
+				'dark': '#0B0D17',
+				'light-blue': '#D0D6F9',
+			},
+			fontFamily:{
+				barlow: ['var(--font-barlow)'],
+				barlowCondensed: ['var(--font-barlow-condensed)'],
+				bellefair: ['var(--font-bellefair)']
+			}
     },
   },
   plugins: [],
