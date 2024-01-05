0d37f6aa495a9c59255294bde510c9d0714f5028
Juliendy Mendes <juliendy0708@gmail.com>
2024-01-05 14:57:22 -0400

    feat: finalized home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Space Tourism</h1>
+    <main className="min-h-screen bg-home-bg-mobile bg-no-repeat bg-center bg-[length:100%_100%] font-normal text-center flex flex-col justify-around items-center">
+      <section>
+        <p className="font-barlowCondensed text-base font-normal uppercase tracking-[2.7px] text-light-blue">
+          So, you want to travel to{" "}
+          <span className="font-bellefair text-[80px] leading-[100px] uppercase tracking-normal text-white">
+            space
+          </span>
+        </p>
+        <p className="text-light-blue font-barlow text-base text-center leading-6	max-w-[90%] m-auto">
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </p>
+      </section>
+
+      <Link
+        href="#"
+        className="text-dark font-bellefair text-center text-[20px] tracking-[1.25px] uppercase leading-[150px] bg-white h-[150px] w-[150px] rounded-full block"
+      >
+        Explore
+      </Link>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "home-bg-mobile": "url('/home/background-home-mobile.jpg')",
       },
       colors: {
         dark: "#0B0D17",
